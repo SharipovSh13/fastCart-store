d40e027266e65c221e690676f3c0065a21da598a
shuhrat <shyhratyagnobi2002@gmail>
2025-06-13 11:45:02 +0300

    Function get category + swiper

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,70 @@
 import AsideCategory from "@/widgets/categoryAsideBar/categoryAside";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
 
-export default function Home(){
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+export default function Home() {
     return <>
-    <section aria-label="Главная меню и слайдер карусел">
-        <div className=" flex">
-            <div className=" bg-red-500">
-                <AsideCategory/>
-            </div>
-            <div className="">
+        <section aria-label="Главная меню и слайдер карусел" className="w-[99.9%] m-auto">
+            <div className=" flex w-[98%] m-auto p-2">
+                <div className="w-44 m-auto">
+                    <AsideCategory />
+                </div>
+                <div className="w-[70%] h-fit m-auto   flex items-center justify-items-center relative">
+                    <Swiper
+                        modules={[Autoplay, Pagination]}
 
+                        spaceBetween={20}
+                        slidesPerView={1}
+                        onSlideChange={() => console.log('slide change')}
+                        onSwiper={(swiper) => console.log(swiper)}
+                        pagination={{
+                            clickable: true,
+                            bulletActiveClass: 'swiper-pagination-bullet-active',
+                            dynamicBullets: false,
+
+                        }}
+                        autoplay={{
+
+                            delay: 2500,
+                            disableOnInteraction: true,
+                        }}
+                        style={{
+
+
+                        }}
+
+                    >
+
+                        <SwiperSlide className="w-fit">
+                            <img src="/Frame 560.png" alt="" />
+                            <div className="w-54 h-6 absolute left-[37%] z-1 top-[91%] bg-slate-900 shadow-[0px_0px_5px_0px] rounded-sm shadow-gray-300">
+
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className="h-fit w-fit">
+                            <img src="/Frame 560.png" alt="" />
+                            <div className="w-54 h-6 absolute left-[37%] z-1 top-[91%] bg-slate-900 shadow-[0px_0px_5px_0px] rounded-sm shadow-gray-300">
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className="h-fit w-fit">
+                            <img src="/Frame 560.png" alt="" />
+                            <div className="w-54 h-6 absolute left-[37%] z-1 top-[91%] bg-slate-900 shadow-[0px_0px_5px_0px] rounded-sm shadow-gray-300">
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className="h-fit w-fit">
+                            <img src="/Frame 560.png" alt="" />
+                            <div className="w-54 h-6 absolute left-[37%] z-1 top-[91%] bg-slate-900 shadow-[0px_0px_5px_0px] rounded-sm shadow-gray-300">
+                            </div>
+                        </SwiperSlide>
+
+
+                    </Swiper>
+
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
     </>
 }
--- src/widgets/categoryAsideBar/categoryAside.tsx
+++ src/widgets/categoryAsideBar/categoryAside.tsx
@@ -1,26 +1,66 @@
 import { AppDispatch, RootState } from "@/app/store/store";
 import { getCategory } from "@/entities/category/api/categoryApi";
 import { ICategory } from "@/shared/lib/interface";
+import { AccordionContentCustom, AccordionCustom, AccordionItemCustom, AccordionTriggerCustom } from "@/shared/ui/kit/accordionNoChevron";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/shared/ui/kit/accordion";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
 
 export default function AsideCategory() {
     const dispatch = useDispatch<AppDispatch>();
     const categories = useSelector((state: RootState) => state.categories.data) as ICategory[];
-    
+
     useEffect(() => {
         dispatch(getCategory())
     }, [dispatch]);
     console.log(categories);
     return <>
-        <div>
-            <h1>Категории</h1>
-            {categories.map((cat, index) => {
-                return <h1 key={index}>
-                    {cat.categoryName}
+        <div className="font-stretch-semi-expanded">
+            <Accordion type="single" collapsible defaultValue="item-1" className="border-r-[1.5px] border-r-gray-300" >
+                {categories.slice(0, 2).map((cat, index) => {
+                    return <AccordionItem value={`item_${index}`} key={index} className="border-none">
+                        <AccordionTrigger className="">
+                            {cat.categoryName}
 
-                </h1>
-            })}
+                        </AccordionTrigger>
+                        <AccordionContent>
+                            {cat.subCategories.map((sub, index,) => {
+                                return <h2 key={index} >
+                                    {
+                                        sub.subCategoryName
+                                    }
+                                </h2>
+                            })}
+                        </AccordionContent>
+
+                    </AccordionItem>
+                })}
+            </Accordion>
+
+
+            <AccordionCustom type="single" collapsible>
+                {categories.slice(2).map((cat, index) => {
+                    return <AccordionItemCustom key={index} className="border-b-0 border-r-[1.5px] border-gray-300 " value={`item_${index}`}>
+                        <AccordionTriggerCustom>
+                            {cat.categoryName}  
+
+
+                        </AccordionTriggerCustom>
+                        <AccordionContentCustom>
+                            {cat.subCategories.map((sub, index, ) => {
+                                return <AccordionContent key={index}>
+                                    {
+                                        sub.subCategoryName
+                                    }
+                                </AccordionContent>
+                                
+                              
+                            })}
+                        </AccordionContentCustom>
+                    </AccordionItemCustom>
+                })}
+
+            </AccordionCustom>
 
         </div>
 
