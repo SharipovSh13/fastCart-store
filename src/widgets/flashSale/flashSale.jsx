a6036875cf16f680c74c4c7329ce015c786313cd
shuhrat <shyhratyagnobi2002@gmail>
2025-07-09 23:15:30 +0300

    create Date Nfs in component Flash Sale

--- src/widgets/flashSale/flashSale.jsx
+++ src/widgets/flashSale/flashSale.jsx
@@ -7,19 +7,36 @@ import { ProductCard } from "../../shared/components/productCard/productCard";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
+
+import { differenceInSeconds } from "date-fns";
 import "swiper/css";
 import "swiper/css/navigation";
 export default function FlashSale() {
   const { products } = useSelector((state) => state.products);
 
+  const endOfPromotion = new Date(2025, 6, 22, 23, 59, 59);
+
   const swiperRef = useRef();
+  const [dateState, setDateState] = useState(new Date());
   const [isStart, setIsStart] = useState(true);
   const [isEnd, setIsEnd] = useState(false);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getProducts());
+    const intervalId = setInterval(() => {
+      setDateState((prevDate) => new Date(prevDate.getTime() + 1000));
+    }, 1000);
+
+    return () => clearInterval(intervalId);
   }, [dispatch]);
 
+  const remainingSeconds = differenceInSeconds(endOfPromotion, dateState);
+  const remainingDays = Math.floor(remainingSeconds / (24 * 60 * 60));
+  const remainingHours = Math.floor(
+    (remainingSeconds % (24 * 60 * 60)) / (60 * 60)
+  );
+  const remainingMinutes = Math.floor((remainingSeconds % (60 * 60)) / 60);
+  const remainingSecondsMod = remainingSeconds % 60;
   return (
     <>
       <div className="mt-14 sm:mt-64  md:mt-52 lg:mt-0">
@@ -32,24 +49,24 @@ export default function FlashSale() {
           <div className="flex items-center gap-2 ">
             <div>
               <span className="font-[400]">Days</span>
-              <h2 className="font-bold text-xl">03</h2>
+              <h2 className="font-bold text-xl">{remainingDays}</h2>
             </div>
             <h1 className="font-bold text-2xl text-[#DB4444]">:</h1>
             <div>
               <span className="font-[400]">Hours</span>
-              <h2 className="font-bold text-2xl">23</h2>
+              <h2 className="font-bold text-2xl">{remainingHours}</h2>
             </div>
             <h1 className="font-bold text-2xl text-[#DB4444]">:</h1>
 
             <div>
               <span className="font-[400]">Minutes</span>
-              <h2 className="font-bold text-xl">19</h2>
+              <h2 className="font-bold text-xl">{remainingMinutes}</h2>
             </div>
             <h1 className="font-bold text-2xl text-[#DB4444]">:</h1>
 
             <div>
               <span className="font-[400]">Seconds</span>
-              <h2 className="font-bold text-2xl">56</h2>
+              <h2 className="font-bold text-2xl">{remainingSecondsMod}</h2>
             </div>
           </div>
         </div>
